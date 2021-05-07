32213bc2a41ecabbc6fcba827799973ccdeb53a8
Ginger Alford <gingeralford@gmail.com>
2021-05-06 20:21:13 -0400

    attempting to fix ssl issue

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 // require("dotenv").config();
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require:true,
+            rejectUnauthorized:false,
+        }
+    }
 });
 
 sequelize.authenticate().then(
